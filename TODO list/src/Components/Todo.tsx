5c0ea7e31c3577a4b50ac22af32cabf868c250d1
Aslan Mammadov <mammadovaslan21@gmail.com>
2023-03-31 19:19:23 +0400

    todo titile editability implemented

--- TODO list/src/Components/Todo.tsx
+++ TODO list/src/Components/Todo.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { TodoI } from "../types/types";
 
 const Todo = ({
@@ -12,6 +12,10 @@ const Todo = ({
   setTodos,
 }: TodoI) => {
   const [done, setDone] = useState<boolean>(isDone);
+  const [titleClicked, setTitleClicked] = useState(false);
+  const [titleValue, setTitleValue] = useState(title);
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setDone(e.target.checked);
@@ -28,19 +32,56 @@ const Todo = ({
     setTodos && setTodos((prev) => prev.filter((todo) => todo.id !== id));
   };
 
+  const clickHandler = () => {
+    setTitleClicked(true);
+    inputRef?.current?.focus();
+  };
+
+  const changeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitleValue(e.target.value)
+
+    if (setTodos && todos) {
+      const index = todos.findIndex((todo) => todo.id === id);
+      const newTodos = [...todos];
+      newTodos[index] = { ...newTodos[index], title: e.target.value };
+      setTodos(newTodos);
+    }
+    setTitleClicked(false)
+  };
+
+  const style = {
+    opacity: titleClicked ? "1" : "0",
+    width: titleClicked ? "auto" : "0",
+  };
+
   return (
     <>
-      <div className="typography">
-        <h2 className="todo-title">{title}</h2>
-        <p className="todo-description">{description}</p>
+      <div className="todo-header">
+        {!titleClicked && (
+          <h2 className="todo-title" onClick={clickHandler}>
+            {title}
+          </h2>
+        )}
+        <input
+          type="text"
+          className="title-input"
+          style={style}
+          ref={inputRef}
+          value={titleValue}
+          onChange={(e) => setTitleValue(e.target.value)}
+          onBlur={changeTitle}
+        />
+
+        <div className="buttons">
+          <input type="checkbox" id="done" checked={done} onChange={changeHandler} />
+          <label htmlFor="done"></label>
+          <button className="delete-todo" onClick={removeTodo}>
+            X
+          </button>
+        </div>
       </div>
-      <div className="buttons">
-        <input type="checkbox" id="done" checked={done} onChange={changeHandler} />
-        <label htmlFor="done"></label>
-        <button className="delete-todo" onClick={removeTodo}>
-          X
-        </button>
-      </div>
+      <p className="todo-description">{description}</p>
+      <button className="add-todo-button">Add to-do</button>
     </>
   );
 };
